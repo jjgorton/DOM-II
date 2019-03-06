7aa8bb60782e43593efccdeffb2f645970cf3a31
jjgorton <jjgorton@gmail.com>
2019-03-05 20:04:16 -0500

    have 4 more features to add to reach MVP

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 // Your code goes here
+
+//----------------nav bar changes color on scroll-----------------
+
 const body = document.querySelector('body');
 const navBar = document.querySelector('header');
 
@@ -7,9 +10,15 @@ window.addEventListener('scroll', function (e) {
     navBar.style.opacity = '0.8';
 });
 
-// window.addEventListener("load", function (e) {
-//     alert("Congratulations!  You have found the right traveling partner!");
-// });
+
+//-- -- -- -- -- -- -- -- - load listener gives a pop up message-- -- -- -- -- -- -- -- -- -- --
+
+window.addEventListener("load", function (e) {
+    alert("Congratulations!  You have found the right traveling partner!");
+});
+
+
+//-------------------anchor tags default off------------------------
 
 const aTag = document.querySelectorAll('a');
 
@@ -18,10 +27,10 @@ aTag.forEach(item => item.addEventListener('click', function (e) {
     console.log("a tag default prevented");
 }));
 
+
+//---------------------bottom section cards mouseover change color---------------
+
 const destCard = document.querySelectorAll('.destination');
-// destCard.forEach(item => item.addEventListener('mouseover', function (e) {
-//     destCard.forEach(item => item.style.background = '#17A2B8');
-// }));
 
 destCard[0].addEventListener('mouseenter', function (e) {
     destCard[0].style.background = '#ffaa83';
@@ -57,4 +66,60 @@ destCardContainer[0].addEventListener('mouseleave', function (e) {
     destCard[0].style.background = '#ffffff';
     destCard[1].style.background = '#ffffff';
     destCard[2].style.background = '#ffffff';
+});
+
+//-------------click to change color-------------
+
+destCardContainer[0].addEventListener('click', function (e) {
+    destCardContainer[0].style.background = '#1e7268';
+});
+
+//------------------btn click and stop propogation-----------
+
+const button = document.querySelectorAll('.btn');
+button[0].addEventListener('click', function (e) {
+    destCard[1].style.display = "none";
+    destCard[2].style.display = "none";
+    destCardContainer[0].style.justifyContent = 'center';
+    destCard[0].style.width = '75%';
+    e.stopPropagation();
+});
+button[1].addEventListener('click', function (e) {
+    destCard[0].style.display = "none";
+    destCard[2].style.display = "none";
+    destCardContainer[0].style.justifyContent = 'center';
+    destCard[1].style.width = '75%';
+    e.stopPropagation();
+});
+button[2].addEventListener('click', function (e) {
+    destCard[0].style.display = "none";
+    destCard[1].style.display = "none";
+    destCardContainer[0].style.justifyContent = 'center';
+    destCard[2].style.width = '75%';
+    e.stopPropagation();
+});
+
+
+//---------------double click to revert card back-----------
+
+button[0].addEventListener('dblclick', function (e) {
+    destCard[1].style.display = "block";
+    destCard[2].style.display = "block";
+    destCardContainer[0].style.justifyContent = 'space-between';
+    destCard[0].style.width = '30%';
+    e.stopPropagation();
+});
+button[1].addEventListener('dblclick', function (e) {
+    destCard[0].style.display = "block";
+    destCard[2].style.display = "block";
+    destCardContainer[0].style.justifyContent = 'space-between';
+    destCard[1].style.width = '30%';
+    e.stopPropagation();
+});
+button[2].addEventListener('dblclick', function (e) {
+    destCard[0].style.display = "block";
+    destCard[1].style.display = "block";
+    destCardContainer[0].style.justifyContent = 'space-between';
+    destCard[2].style.width = '30%';
+    e.stopPropagation();
 });
